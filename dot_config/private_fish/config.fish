c9edc2266db1e283daf8f070785fb93f6032b02e
Riddhiman Saha <riddhiman1014@gmail.com>
2026-02-10 11:13:17 -0500

    Add default $EDITOR for bash and fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,6 +2,16 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Set default editor
+if type -q nvim
+    set -gx EDITOR nvim
+else if type -q vim
+    set -gx EDITOR vim
+else
+    set -gx EDITOR nano
+end
+
 # Set up fzf key bindings
 fzf --fish | source
+# zoxide - fuzzy alternative of cd
 zoxide init fish | source
